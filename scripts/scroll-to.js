33dbdd87a715c6088e1d2e724c53932198e4b8e9
Ryszard Bosiak <ryszard.bosiak@outlook.com>
2018-07-16 19:23:21 +0200

    JB Adding ScrollTo button, initial script implementation

---
+++ scripts/scroll-to.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+const scrollButton = $('.scroll-down');
+
+scrollButton.on("click", function(e) {
+		e.preventDefault();
+		$("body, html").animate({ 
+				scrollTop: $( $(this).attr('href') ).offset().top 
+		}, 600);
+		
+	});
+});
