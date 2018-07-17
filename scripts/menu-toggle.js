d618ddc000fcaf64d63f0ffbe3c7a3bc79835eea
Ryszard Bosiak <ryszard.bosiak@outlook.com>
2018-07-17 12:24:44 +0200

    JB Mobile menu implementation

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -5,12 +5,25 @@ $(document).ready(function () {
 	const navigationWrapper = $('.navigation-wrapper');
 	const body = $('body');
 	const menuBackground = $('.mobile-menu-background');
+	const mobileWidth = 768;
+	// const window = $(window);
 
 	function toggleMenu() {
 		body.toggleClass('show-menu');
 		menuBackground.fadeToggle(300);
 	}
 
+	function closeMobileMenu() {
+		body.removeClass('show-menu');
+		menuBackground.fadeOut(300);
+	}
+
 	menuOpener.on('click', toggleMenu);
 	menuBackground.on('click', toggleMenu);
+
+	$(window).resize(function() {
+		if ($(window).width() >= mobileWidth) {
+			closeMobileMenu();
+		} 
+	});
 });
