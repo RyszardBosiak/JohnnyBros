3a7265ced83d9b11ab60c131cf72c46635e6c85f
Ryszard Bosiak <ryszard.bosiak@outlook.com>
2018-07-19 19:52:14 +0200

    JB Code refactoring, mobile fixes

--- scripts/menu-transparency.js
+++ scripts/menu-transparency.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
 	
 	function checkMEnuPosition() {
 		var scroll = $(window).scrollTop();
+		const navLogoBox = $('.nav-logo-box');
 		const nav = $('nav');
 		if (scroll >= 100) {
+			navLogoBox.addClass('nav-dark');
 			nav.addClass('nav-dark');
+			navLogoBox.addClass('element-shadow');
 		} else {
+			navLogoBox.removeClass('nav-dark');
 			nav.removeClass('nav-dark');
+			navLogoBox.removeClass('element-shadow');
 		}
 	}
 	
